05b189fdbbd265401079158b7e504e95e0fff5f8
vipuljad3 <vipuljad3@outlook.com>
2021-04-15 18:04:29 +1000

    Add files via upload

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
-from  .models import Job
+from  .models import Job, Comment
 # Register your models here.
 admin.site.register(Job)
+
+admin.site.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'created_on', 'active')
+    list_filter = ('active', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(active=True)
